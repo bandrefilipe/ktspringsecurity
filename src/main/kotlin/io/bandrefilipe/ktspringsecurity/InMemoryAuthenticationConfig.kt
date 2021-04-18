879b99a2c1bb26fed63b53ee0c994e7cb949f14f
Andre Barranco <b.andrefilipe@gmail.com>
2021-04-17 22:39:43 -0300

    Configura autenticador in-memory

---
+++ src/main/kotlin/io/bandrefilipe/ktspringsecurity/InMemoryAuthenticationConfig.kt
@@ -0,0 +1,40 @@
+package io.bandrefilipe.ktspringsecurity
+
+import mu.KotlinLogging
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Profile
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.crypto.password.NoOpPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+
+@Profile("in-memory-authentication")
+@EnableWebSecurity
+class InMemoryAuthenticationConfig : WebSecurityConfigurerAdapter() {
+
+    init {
+        log.debug { "Creating bean ${InMemoryAuthenticationConfig::class.simpleName}" }
+    }
+
+    override fun configure(auth: AuthenticationManagerBuilder) {
+        log.debug { "Configuring in-memory authentication" }
+        auth.inMemoryAuthentication()
+            .withUser("admin")
+            .password("admin")
+            .roles("ADMIN", "USER")
+            .and()
+            .withUser("user")
+            .password("user")
+            .roles("USER")
+    }
+
+    @Bean
+    fun noOpPasswordEncoder(): PasswordEncoder {
+        val bean = NoOpPasswordEncoder.getInstance()
+        log.debug { "Creating bean ${bean::class.simpleName}" }
+        return bean
+    }
+}
+
+private val log = KotlinLogging.logger {}
