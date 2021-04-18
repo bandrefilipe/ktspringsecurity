22b3800898bc0cc831f567b0cb093b7d8c0be2ad
Andre Barranco <b.andrefilipe@gmail.com>
2021-04-17 21:52:58 -0300

    Cria projeto

---
+++ src/main/kotlin/io/bandrefilipe/ktspringsecurity/KtSpringSecurityApplication.kt
@@ -0,0 +1,11 @@
+package io.bandrefilipe.ktspringsecurity
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KtSpringSecurityApplication
+
+fun main(args: Array<String>) {
+    runApplication<KtSpringSecurityApplication>(*args)
+}
---
+++ src/test/kotlin/io/bandrefilipe/ktspringsecurity/KtSpringSecurityApplicationTests.kt
@@ -0,0 +1,12 @@
+package io.bandrefilipe.ktspringsecurity
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KtSpringSecurityApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
